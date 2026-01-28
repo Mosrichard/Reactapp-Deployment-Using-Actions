ddd8bd4f1df1d14264b18d0b16b21b58ee795c0d
mosrichard <mosrichard3@gmail.com>
2026-01-28 16:03:42 +0530

    updated tf code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,14 @@
+# Use default VPC
 data "aws_vpc" "default" {
   default = true
 }
+
 data "aws_security_group" "default" {
   name   = "default"
   vpc_id = data.aws_vpc.default.id
 }
+
+# Allow HTTP (port 80)
 resource "aws_security_group_rule" "allow_http_default_sg" {
   type              = "ingress"
   from_port         = 80
@@ -14,20 +18,42 @@ resource "aws_security_group_rule" "allow_http_default_sg" {
   security_group_id = data.aws_security_group.default.id
 }
 
+# Allow SSH (port 22)
+resource "aws_security_group_rule" "allow_ssh_default_sg" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_security_group.default.id
+}
+
+# EC2 Instance
 resource "aws_instance" "webserver" {
   ami           = var.ami_value
   instance_type = var.instance_type_value
   key_name      = var.Key_pair
 
+  # Associate default SG
   vpc_security_group_ids = [
     data.aws_security_group.default.id
   ]
+
+  # Public IP required for SSH
+  associate_public_ip_address = true
 
   tags = {
     Name = var.server_name
   }
 }
 
+# Terraform S3 bucket for backend
 resource "aws_s3_bucket" "s3_bucket" {
-    bucket = "backend-statefile-tf-github-actions-cicd"
+  bucket = "backend-statefile-tf-github-actions-cicd"
+}
+
+# Output EC2 public IP
+output "public_ip" {
+  value       = aws_instance.webserver.public_ip
+  description = "Public IP of the EC2 instance"
 }

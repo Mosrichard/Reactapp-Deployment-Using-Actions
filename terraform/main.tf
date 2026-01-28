c7b5470e3e32e58a1d6e15004909396685eddca2
mosrichard <mosrichard3@gmail.com>
2026-01-28 16:05:44 +0530

    updated tf code op

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,8 +52,4 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = "backend-statefile-tf-github-actions-cicd"
 }
 
-# Output EC2 public IP
-output "public_ip" {
-  value       = aws_instance.webserver.public_ip
-  description = "Public IP of the EC2 instance"
-}
+
